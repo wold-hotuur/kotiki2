636277392bc4c1384721fbe3d57b907f7c392e92
wold-hotuur <105804700+wold-hotuur@users.noreply.github.com>
2022-11-18 11:57:50 +0900

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === '') {
+      myImage.setAttribute ('src','');
+    } else {
+      myImage.setAttribute ('src','');
+    }
+}
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+function setUserName() {
+    var myName = prompt('Введите свое имя');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Добро пожаловать ' + myName;
+  }
+  if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Привет ' + storedName;
+  }
+  myButton.onclick = function() {
+    setUserName();
+  }
